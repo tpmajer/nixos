20ae9175260cd71d8f74b76b69d4651cb8c41b5d
Tomasz Majer <17414256+tpmajer@users.noreply.github.com>
2026-04-07 23:18:16 +0200

    chore(system): remove unused flatpak-repo.service

--- system.nix
+++ system.nix
@@ -96,17 +96,6 @@
     dockerCompat = true;
   };
 
-  # systemd.services = {
-  #   flatpak-repo = {
-  #     wantedBy = [ "multi-user.target" ];
-  #     path = [ pkgs.flatpak ];
-  #     script = "
-  #       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-  # 	  flatpak remote-add --if-not-exists gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
-  #     ";
-  #   };
-  # };
-
   services = {
     dbus.implementation = "broker";
     hypridle.enable = true;
